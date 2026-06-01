1223e427bcf2595ac87a1609d007fe52a67de5c4
thiagocoder30 <thiagocoder30@gmail.com>
2026-06-01 14:42:34 +0000

    feat: add sprint 175 final paper certification report

---
+++ src/domain/certification/final-paper-certification-report/index.ts
@@ -0,0 +1,11 @@
+export {
+  FinalPaperCertificationReportEngine,
+  type FinalPaperCertificationDecision,
+  type FinalPaperCertificationEvaluation,
+  type FinalPaperCertificationEvidence,
+  type FinalPaperCertificationInput,
+  type FinalPaperCertificationMetrics,
+  type FinalPaperCertificationPolicy,
+  type FinalPaperCertificationReason,
+  type FinalPaperCertificationResult,
+} from './final-paper-certification-report-engine';
